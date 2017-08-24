b51f1174d1c857c250fb1de6e5e635d7a2f40a78
Jim Tsai <jtsai@distroscale.com>
2017-08-24 14:51:21 -0700

    add perspective projection

---
+++ lesson_4/model.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "model.h"
+
+Model::Model(const char *filename) : verts_(), faces_() {
+    std::ifstream in;
+    in.open (filename, std::ifstream::in);
+    if (in.fail()) return;
+    std::string line;
+    while (!in.eof()) {
+        std::getline(in, line);
+        std::istringstream iss(line.c_str());
+        char trash;
+        if (!line.compare(0, 2, "v ")) {
+            iss >> trash;
+            Vec3f v;
+            for (int i=0;i<3;i++) iss >> v.raw[i];
+                // std::cout << v;
+
+            verts_.push_back(v);
+        } else if (!line.compare(0, 3, "vt ")) {
+            iss >> trash >> trash;
+            Vec3f v;
+            for (int i=0;i<3;i++) iss >> v.raw[i];
+            // std::cout << v;
+            texture_verts_.push_back(v);
+        }else if (!line.compare(0, 2, "f ")) {
+            std::vector<int> f;
+            std::vector<int> ft;
+            int itrash, idx, vtext;
+            iss >> trash;
+            while (iss >> idx >> trash >> vtext >> trash >> itrash) {
+                idx--; // in wavefront obj all indices start at 1, not zero
+                vtext--;
+                f.push_back(idx);
+                ft.push_back(vtext);
+            }
+            faces_.push_back(f);
+            texture_faces_.push_back(ft);
+        }
+    }
+    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
+}
+
+Model::~Model() {
+}
+
+int Model::nverts() {
+    return (int)verts_.size();
+}
+
+int Model::nfaces() {
+    return (int)faces_.size();
+}
+
+std::vector<int> Model::face(int idx) {
+    return faces_[idx];
+}
+
+std::vector<int> Model::texture_face(int idx) {
+    return texture_faces_[idx];
+}
+
+Vec3f Model::vert(int i) {
+    return verts_[i];
+}
+
+Vec3f Model::texture_vert(int i) {
+    return texture_verts_[i];
+}
