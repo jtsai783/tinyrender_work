08a52903fbaebbb6324bef9c6c30ca6c78751182
Jim Tsai <jtsai@distroscale.com>
2017-09-02 13:52:45 -0700

    save point

--- lesson_6/main.cpp
+++ lesson_6/main.cpp
@@ -6,15 +6,15 @@
 #include "model.h"
 #include "matrix.h"
 #include "our_gl.h"
-// #include <algorithm>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
 Model *model = NULL;
 TGAImage *texture_img = NULL;
-const int width  = 800;
-const int height = 800;
+
+const int width  = 2000;
+const int height = 2000;
 
 const Vec3f camera(1, 1, 3);
 const Vec3f center(0, 0, 0);
@@ -51,7 +51,6 @@ Vec3f transform_p(Matrix m, Vec3f v){
 
 struct Shader : public IShader {
     Vec3f texture_coords[3];
-    // Vec3f normal_vec[3];
     Matrix cob;
     Matrix cob_IT;
 
@@ -59,19 +58,61 @@ struct Shader : public IShader {
     Matrix M_IT;
 
     Vec3f triangle_normal;
+    Vec3f vertex_normal[3];
     Vec3f verts[3];
 
-    virtual Vec3f vertex(int nface, int nthvert){
+    Matrix A = Matrix(3,3);
+
+    Matrix iA = Matrix(3,1);
+    Matrix jA = Matrix(3,1);
+
+    virtual Vec3f vertex(int nface, int nthvert, TGAImage &image){
         std::vector<int> face = model->face(nface);
         Vec3f v = model->vert(face[nthvert * 3]);
         Vec3f thisVert = transform_p(cob, v);
         texture_coords[nthvert] = model->texture_vert(face[nthvert * 3 + 1]);
+        Vec3f normal = model->normal_vert(face[nthvert * 3 + 2]);
+        vertex_normal[nthvert] = transform_v(M_IT, normal);
         verts[nthvert] = v;
         if(nthvert == 2){
-           triangle_normal = cross2((verts[0] - verts[1]), (verts[0] - verts[2]));
+           //calculate the A matrix
+            Vec3f p0p1 = transform_p(M,verts[1]) - transform_p(M,verts[0]);
+            Vec3f p0p2 = transform_p(M,verts[2]) - transform_p(M,verts[0]);
+            // Vec3f p0p1 = verts[1] - verts[0];
+            // Vec3f p0p2 = verts[2] - verts[0];
+            A(0,0) = p0p1.x;
+            A(0,1) = p0p1.y;
+            A(0,2) = p0p1.z;
+
+            A(1,0) = p0p2.x;
+            A(1,1) = p0p2.y;
+            A(1,2) = p0p2.z;
+
+            A(2,0) = vertex_normal[2].x;
+            A(2,1) = vertex_normal[2].y;
+            A(2,2) = vertex_normal[2].z;
+
+            iA(0,0) = texture_coords[1].x - texture_coords[0].x;
+            iA(1,0) = texture_coords[2].x - texture_coords[0].x;
+            iA(2,0) = 0;
+
+            jA(0,0) = texture_coords[1].y - texture_coords[0].y;
+            jA(1,0) = texture_coords[2].y - texture_coords[0].y;
+            jA(2,0) = 0;
+
+            Matrix i = A.inverse() * iA;
+            Matrix j = A.inverse() * jA;
+
+            Vec3f i_vec(i(0,0), i(0,1), i(0,2));
+            i_vec = i_vec.normalize() * 30;
+
+            Vec2i p0 = Vec2i((int)thisVert.x, (int)thisVert.y);
+            Vec2i p1 = p0 + Vec2i(i_vec.x, i_vec.y);
+
+            line(p0, p1, image, green);
+
+
         }
-        // normal_vec[nthvert] = model->normal_vert(face[nthvert * 3 + 2]);
-        // normal_vec[nthvert] = transform_v(M_IT, normal_vec[nthvert]);
         return thisVert;
     }
 
@@ -85,44 +126,61 @@ struct Shader : public IShader {
         texture_y = texture_y * (model->diffuse).get_height();
         color = (model->diffuse).get(roundf(texture_x), roundf(texture_y));
 
-        //interpolate the vectors
-        // float normal_x = bc[0] * normal_vec[0].x + bc[1] * normal_vec[1].x + bc[2] * normal_vec[2].x;
-        // float normal_y = bc[0] * normal_vec[0].y + bc[1] * normal_vec[1].y + bc[2] * normal_vec[2].y;
-        // float normal_z = bc[0] * normal_vec[0].z + bc[1] * normal_vec[1].z + bc[2] * normal_vec[2].z;
-        // Vec3f normal_vec = Vec3f(normal_x, normal_y, normal_z);
 
-        // normal_x = ((normal_vec.normalize() + Vec3f(1.0,1.0,1.0)) * 0.5 * 255).x;
-        // normal_y = ((normal_vec.normalize() + Vec3f(1.0,1.0,1.0)) * 0.5 * 255).y;
-        // normal_z = ((normal_vec.normalize() + Vec3f(1.0,1.0,1.0)) * 0.5 * 255).z;
 
-        // TGAColor normal_color = TGAColor(roundf(normal_x), roundf(normal_y), roundf(normal_z), 255);
+        //setup A
+        Vec3f n = vertex_normal[0] * bc[0] + vertex_normal[1] * bc[1] + vertex_normal[2] * bc[2];
+        // n = n.normalize();
 
-        // color = normal_color;
+        // A(2,0) = n.x;
+        // A(2,1) = n.y;
+        // A(2,2) = n.z;
 
-        TGAColor normal_color = (model->normal_map).get(roundf(texture_x), roundf(texture_y));
-        Vec3f normal_vec = Vec3f(normal_color.r, normal_color.g, normal_color.b);
-        normal_vec = normal_vec * (1.0/255.0) * 2.0 - Vec3f(1.0,1.0,1.0);
+        // Matrix i = A.inverse() * iA; 
+        // Matrix j = A.inverse() * jA;
+
+        // //read tangent space normal from map
+        // TGAColor normal_color = (model->normal_map).get(roundf(texture_x), roundf(texture_y));
+        // Vec3f normal_vec = Vec3f(normal_color.r, normal_color.g, normal_color.b);
+        // normal_vec = normal_vec * (1.0/255.0) * 2.0 - Vec3f(1.0,1.0,1.0);
+
+        // Matrix Tan = Matrix(3,3);
+        // Tan(0,0) = i(0,0);
+        // Tan(0,1) = i(0,1);
+        // Tan(0,2) = i(0,2);
+
+        // Tan(1,0) = j(0,0);
+        // Tan(1,1) = j(0,1);
+        // Tan(1,2) = j(0,2);
+
+        // Tan(2,0) = n.x;
+        // Tan(2,1) = n.y;
+        // Tan(2,2) = n.z;
+
+        // n = transform_v(Tan.transpose(), normal_vec);
+
+        // n = (n + Vec3f(1.0,1.0,1.0)) * 0.5 * 255.0;
+
+        // std::cout << n << std::endl;
+
+        // color = TGAColor(n.x, n.y, n.z, 255);
 
         Vec3f temp_light = transform_v(M, light_dir);
-        Vec3f temp_norm = transform_v(M_IT, normal_vec);
+        // Vec3f temp_norm = transform_v(M_IT, normal_vec);
 
-        // normal_vec = transform_p((Viewport * ModelView).inverse().transpose(), normal_vec);
+        // float intensity = std::max(0.f,  temp_norm.normalize() * temp_light.normalize());
 
-        // light_dir = transform_v(cob, light_dir);
-        // normal_vec = transform_v(cob_IT, normal_vec);
 
-        // float intensity = std::max(0.f,  light_dir.normalize() * normal_vec.normalize());
+        float intensity = std::max(0.f,  n.normalize() * temp_light.normalize());
 
-        // triangle_normal = transform_v(ModelView.inverse().transpose(), triangle_normal);
-        // light_dir = transform_v(ModelView, light_dir);
-
-        float intensity = std::max(0.f,  temp_norm.normalize() * temp_light.normalize());
+        // float intensity = 1;
 
         color = TGAColor((float)color.r * intensity, (float)color.g * intensity, (float)color.b * intensity, 255);
 
         return false;
     }
 };
+
 
 int main(int argc, char** argv) {
     if (2==argc) {
@@ -131,23 +189,15 @@ int main(int argc, char** argv) {
         model = new Model("obj/african_head.obj");
     }
 
-    // TGAImage texture_img(width, height, TGAImage::RGB);
-
-    // texture_img.read_tga_file("african_head_diffuse.tga");
-    // texture_img.flip_vertically();
 
     TGAImage image(width, height, TGAImage::RGB);
     TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 
+    // image = TGAImage(width, height, TGAImage::RGB);
+
     view(center, camera, up);
     proj(-1.0/(camera - center).norm());
     clip(width/8, height/8, width*3/4, height*3/4);
-    // clip(0, 0, width, height);
-    // light_dir.normalize();
-
-
-    // int *zbuffer = new int[width*height];
-    // for (int i=width*height; i--; zbuffer[i] = 0);
 
 
     Shader shader;
@@ -161,11 +211,10 @@ int main(int argc, char** argv) {
         std::vector<int> face = model->face(i);
         Vec3f screen_coords[3]; 
         for (int j=0; j<3; j++) { 
-            screen_coords[j] = shader.vertex(i, j);
+            screen_coords[j] = shader.vertex(i, j, image);
         }
         triangle(zbuffer, screen_coords, image, shader); 
     }
-
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     zbuffer.flip_vertically();
--- lesson_6/our_gl.h
+++ lesson_6/our_gl.h
@@ -15,9 +15,11 @@ void clip(int x, int y, int width, int height);
 
 void proj(float coeff);
 
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color);
+
 struct IShader {
     virtual ~IShader();
-    virtual Vec3f vertex(int nface, int nthvert) = 0;
+    virtual Vec3f vertex(int nface, int nthvert, TGAImage &image) = 0;
     virtual bool fragment(float *bc, TGAColor &color) = 0;
 };
 
